8b6860ab76779976e194d97a342cd33963df46ce
DevHyung <khuphj@gmail.com>
2018-04-24 00:15:40 +0900

    Add 크로스 부분 완료

--- main.py
+++ main.py
@@ -1,9 +1,64 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 header = {
-'Cookie':"__cfduid=d456afab2c873b2d2b9f78c96432e276c1524302950; newsession=ZJdpHDlH%2FO%2BVM8G8IYXjdrB6LNYnWxtKe3QSwJOFpKbSx%2B7V1ld6ubvo471gffiA4aW70ni9lUATYRMTBE88Bw%2BQo6Rb4HqFDea06logQG7DjfCp%2BHYa4mJ0wB%2FGPlcRz9tlQTC1FdifWvrwkW7lCFnXwspTN3poCab8AzmMmIc4zy7QZOnyeuSeNqZP9zngAHkBVRB4V%2F9ajvMMa%2Fxq4URK15HdHp75X68rLxG7CwJeTXfQuZfHyuyaG4PhksvXZwuNdBgiNqTXo78xoOwHAfckSKjh8zc3a9gqzI7tSmrZIbsrQkZarJ4xBP1fKVFXNXBzsDkcUdD6%2B3s6tjzY5lxl7k5mgWatyFFmUSa512Qg%2B8TD3oxkJkDeQAsH1Jm7DQLYeMSgjVY%2BxD7CbZuFAY4P9LAhbNVJ53BN4zZf%2BPI%3D7302345b30bb8b150030c34eceea762ed46b3d16"
+'Cookie': "",
+'Upgrade-Insecure-Requests':'1',
+'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
+'Accept-Encoding':'gzip, deflate',
+'Accept-Language':'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
 }
+def GetCookie():
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get('http://made-club.com/login')
+    driver.find_element_by_xpath('//*[@id="loginFrm"]/div[1]/input').send_keys('Vie111')
+    driver.find_element_by_xpath('//*[@id="loginFrm"]/div[2]/input').send_keys('1234')
+    driver.find_element_by_xpath('//*[@id="btn_login"]').click()
+    time.sleep(3)
+    str = '__cfduid=' + driver.get_cookies()[0]['value'] + ';' + 'newsession=' + driver.get_cookies()[1]['value']
+    driver.quit()
+    return str
 if __name__=="__main__":
-    html = requests.get('http://made-club.com/game/result?type=sport&page=240',headers=header)
-    bs4 = BeautifulSoup(html.text,'lxml')
-    print( bs4.prettify())
+    saveCookie = open('SESSION.txt').read()
+    header['Cookie'] = saveCookie
+    html = requests.get('http://made-club.com/game/result?type=sport&page=240', headers=header)
+    if '<!-- 신규 가입시 환영 메세지 얼럿 --->' in html.text: # 세션끊긴거
+        print('>>> Session Re Get .. ')
+        cookie = GetCookie()
+        f = open('SESSION.txt', 'w')
+        f.write(cookie)
+        header['Cookie'] = cookie
+    # 크로스경기
+    html = requests.get('http://made-club.com/sports/cross', headers=header)
+    bs4 = BeautifulSoup(html.text, 'lxml')
+    table = bs4.find('table',id='gameListTable')
+    trs = table.find_all('tr')
+    for tr in trs:
+        try:
+            league = tr.find('th').get_text().strip()
+            #print(league)
+        except:
+            pass
+        try:
+            tc = tr.find('td',class_='tc').get_text()
+            ts = tr.find_all('td')[1]['data-ts']
+            leagueType = tr.find_all('td')[1]['data-league_type']
+            homeTeam = tr.find_all('td')[1]['data-team']
+            homeRatio = tr.find_all('td')[1]['data-odds']
+            drawRatio = tr.find_all('td')[4]['data-odds']
+            awayRatio = tr.find_all('td')[5]['data-odds']
+            awayTeam = tr.find_all('td')[5]['data-team']
+            print('{')
+            print('"League":"',league,'",')
+            print('"DateTime":"',tc,'",')
+            print('"LeagueType":"',leagueType,'",')
+            print('"HomeTeam":"',homeTeam,'",')
+            print('"HomeRatio":"',homeRatio,'",')
+            print('"DrawRatio":"',drawRatio,'",')
+            print('"AwayRatio":"',awayRatio,'",')
+            print('"data-ts":"',ts,'",')
+            print('},')
+        except:
+            pass
+
