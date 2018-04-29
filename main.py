7d38322474f233d91974d1459842226632e75385
DevHyung <khuphj@gmail.com>
2018-04-29 11:12:51 +0900

    Backup All

--- main.py
+++ main.py
@@ -14,56 +14,299 @@ header = {
 def GetCookie():
     driver = webdriver.Chrome('./chromedriver')
     driver.get('http://made-club.com/login')
-    driver.find_element_by_xpath('//*[@id="loginFrm"]/div[1]/input').send_keys('Vie111')
-    driver.find_element_by_xpath('//*[@id="loginFrm"]/div[2]/input').send_keys('1234')
+    driver.find_element_by_xpath('//*[@id="loginFrm"]/div[1]/input').send_keys(ID)
+    driver.find_element_by_xpath('//*[@id="loginFrm"]/div[2]/input').send_keys(PW)
     driver.find_element_by_xpath('//*[@id="btn_login"]').click()
     time.sleep(3)
     str = '__cfduid=' + driver.get_cookies()[0]['value'] + ';' + 'newsession=' + driver.get_cookies()[1]['value']
-    #driver.quit()
+    driver.quit()
     return str
 if __name__=="__main__":
-    saveCookie = open('SESSION.txt').read()
-    header['Cookie'] = saveCookie
-    html = requests.get('http://made-club.com/game/result?type=sport&page=240', headers=header)
-    jsonList = []
-    if '<!-- 신규 가입시 환영 메세지 얼럿 --->' in html.text: # 세션끊긴거
-        print('>>> Session Re Get .. ')
-        cookie = GetCookie()
-        f = open('SESSION.txt', 'w')
-        f.write(cookie)
-        header['Cookie'] = cookie
-    # 크로스경기
-    html = requests.get('http://made-club.com/sports/cross', headers=header)
-    bs4 = BeautifulSoup(html.text, 'lxml')
-    table = bs4.find('table',id='gameListTable')
-    trs = table.find_all('tr')
-    for tr in trs:
-        data = {}
-        try:
-            league = tr.find('th').get_text().strip()
-        except:
-            pass
-        try:
-            tc = tr.find('td',class_='tc').get_text()
-            ts = tr.find_all('td')[1]['data-ts']
-            leagueType = tr.find_all('td')[1]['data-league_type']
-            homeTeam = tr.find_all('td')[1]['data-team']
-            homeRatio = tr.find_all('td')[1]['data-odds']
-            drawRatio = tr.find_all('td')[4]['data-odds']
-            awayRatio = tr.find_all('td')[5]['data-odds']
-            awayTeam = tr.find_all('td')[5]['data-team']
-            data["League"] = league
-            data["DateTime"] = tc
-            data["LeagueType"] = leagueType
-            data["HomeTeam"] = homeTeam
-            data["HomeRatio"] = homeRatio
-            data["DrawRatio"] = drawRatio
-            data["AwayRatio"] = awayRatio
-            data["data-ts"] = ts
-            jsonList.append(data)
-        except:
-            pass
-    f = open('JSON.txt','w',encoding='utf8')
-    f.write(json.dumps(jsonList, ensure_ascii=False))
-    print(json.dumps(jsonList, ensure_ascii=False))
+    configLines = open('CONFIG.txt').readlines()
+    ID = configLines[0].split(':')[1].strip()
+    PW = configLines[1].split(':')[1].strip()
+    DELAY = int(configLines[2].split(':')[1].strip())
+    while True:
+        now = time.localtime()
+        s = "%02d/%02d" % (now.tm_mon, now.tm_mday)
+        saveCookie = open('SESSION.txt').read()
+        header['Cookie'] = saveCookie
+        html = requests.get('http://made-club.com/sports/cross', headers=header)
+        root = {}
+        if '<!-- 신규 가입시 환영 메세지 얼럿 --->' in html.text: # 세션끊긴거
+            print('>>> Session Re Get .. ')
+            cookie = GetCookie()
+            f = open('SESSION.txt', 'w')
+            f.write(cookie)
+            header['Cookie'] = cookie
+
+        #경기결과1
+        print('>>> RESULT1 시작')
+        jsonList = []
+        jsonList.clear()
+        page = '&page='
+        pageIdx = 0
+        url = 'http://made-club.com/game/result?type=sport'
+        LoopGo = True
+        while LoopGo:
+            if pageIdx == 0:
+                html = requests.get(url, headers=header)
+            else:
+                html = requests.get(url+page+str(pageIdx*30), headers=header)
+            pageIdx += 1
+            bs4 = BeautifulSoup(html.text, 'lxml')
+            table = bs4.find('table', class_='table_1 non_cursor')
+            trs = table.find_all('tr')
+            for tr in trs:
+                data = {}
+                try:
+                    tc = tr.find('td', class_='tc').get_text().strip()
+                    if s == tc[:5]:
+                        gameType = tr.find_all('td',class_='tc')[1].get_text().strip()
+                        league = tr.find_all('td')[2].get_text().strip()
+                        homeTeam = tr.find_all('td')[3].find('td',class_='teamName').get_text().strip()
+                        homeRatio = tr.find_all('td')[3].find('td',class_='odd').get_text().strip()
+                        drawRatio = tr.find_all('td')[6].get_text().strip()
+                        awayTeam = tr.find_all('td')[7].find('td', class_='teamName').get_text().strip()
+                        awayRatio = tr.find_all('td')[7].find('td', class_='odd').get_text().strip()
+                        homeScore,awayScore = tr.find_all('td')[10].get_text().strip().split(':')
+                        gameResult = tr.find_all('td')[11].get_text().strip()
+                        data["DateTime"] = tc
+                        data["GameType"] = gameType
+                        data["League"] = league
+                        data["HomeTeam"] = homeTeam
+                        data["HomeRatio"] = homeRatio
+                        data["DrawRatio"] = drawRatio
+                        data["AwayRatio"] = awayRatio
+                        data["AwayTeam"] = awayTeam
+                        data['HomeScore'] = homeScore.strip()
+                        data['AwayScore'] = awayScore.strip()
+                        data['GameResult'] = gameResult
+                        jsonList.append(data)
+                    else:
+                        LoopGo = False
+                        break
+                except:
+                    pass
+        root['result1'] = jsonList
+        # 경기결과2
+        print('>>> RESULT2 시작')
+        jsonList = []
+        jsonList.clear()
+        page = '&page='
+        pageIdx = 0
+        url = 'http://made-club.com/game/result?type=special'
+        LoopGo = True
+        while LoopGo:
+            if pageIdx == 0:
+                html = requests.get(url, headers=header)
+            else:
+                html = requests.get(url + page + str(pageIdx * 30), headers=header)
+            pageIdx += 1
+            bs4 = BeautifulSoup(html.text, 'lxml')
+            table = bs4.find('table', class_='table_1 non_cursor')
+            trs = table.find_all('tr')
+            for tr in trs:
+                data = {}
+                try:
+                    tc = tr.find('td', class_='tc').get_text().strip()
+                    if s == tc[:5]:
+                        gameType = tr.find_all('td', class_='tc')[1].get_text().strip()
+                        league = tr.find_all('td')[2].get_text().strip()
+                        homeTeam = tr.find_all('td')[3].find('td', class_='teamName').get_text().strip()
+                        homeRatio = tr.find_all('td')[3].find('td', class_='odd').get_text().strip()
+                        drawRatio = tr.find_all('td')[6].get_text().strip()
+                        awayTeam = tr.find_all('td')[7].find('td', class_='teamName').get_text().strip()
+                        awayRatio = tr.find_all('td')[7].find('td', class_='odd').get_text().strip()
+                        homeScore, awayScore = tr.find_all('td')[10].get_text().strip().split(':')
+                        gameResult = tr.find_all('td')[11].get_text().strip()
+                        data["DateTime"] = tc
+                        data["GameType"] = gameType
+                        data["League"] = league
+                        data["HomeTeam"] = homeTeam
+                        data["HomeRatio"] = homeRatio
+                        data["DrawRatio"] = drawRatio
+                        data["AwayRatio"] = awayRatio
+                        data["AwayTeam"] = awayTeam
+                        data['HomeScore'] = homeScore.strip()
+                        data['AwayScore'] = awayScore.strip()
+                        data['GameResult'] = gameResult
+                        jsonList.append(data)
+                    else:
+                        LoopGo = False
+                        break
+                except:
+                    pass
+        root['result2'] = jsonList
+        # 경기결과3
+        print('>>> RESULT3 시작')
+        jsonList = []
+        jsonList.clear()
+        page = '&page='
+        pageIdx = 0
+        url = 'http://made-club.com/game/result?type=live'
+        LoopGo = True
+        while LoopGo:
+            if pageIdx == 0:
+                html = requests.get(url, headers=header)
+            else:
+                html = requests.get(url + page + str(pageIdx * 30), headers=header)
+            pageIdx += 1
+            bs4 = BeautifulSoup(html.text, 'lxml')
+            table = bs4.find('table', class_='table_1 non_cursor')
+            trs = table.find_all('tr')
+            for tr in trs:
+                data = {}
+                try:
+                    tc = tr.find('td', class_='tc').get_text().strip()
+                    if s == tc[:5]:
+                        gameType = tr.find_all('td', class_='tc')[1].get_text().strip()
+                        league = tr.find_all('td')[2].get_text().strip()
+                        homeTeam = tr.find_all('td')[3].find('td', class_='teamName').get_text().strip()
+                        homeRatio = tr.find_all('td')[3].find('td', class_='odd').get_text().strip()
+                        drawRatio = tr.find_all('td')[6].get_text().strip()
+                        awayTeam = tr.find_all('td')[7].find('td', class_='teamName').get_text().strip()
+                        awayRatio = tr.find_all('td')[7].find('td', class_='odd').get_text().strip()
+                        homeScore, awayScore = tr.find_all('td')[10].get_text().strip().split(':')
+                        gameResult = tr.find_all('td')[11].get_text().strip()
+                        data["DateTime"] = tc
+                        data["GameType"] = gameType
+                        data["League"] = league
+                        data["HomeTeam"] = homeTeam
+                        data["HomeRatio"] = homeRatio
+                        data["DrawRatio"] = drawRatio
+                        data["AwayRatio"] = awayRatio
+                        data["AwayTeam"] = awayTeam
+                        data['HomeScore'] = homeScore.strip()
+                        data['AwayScore'] = awayScore.strip()
+                        data['GameResult'] = gameResult
+                        jsonList.append(data)
+                    else:
+                        LoopGo = False
+                        break
+                except:
+                    pass
+        root['result3'] = jsonList
+        # 크로스경기
+        print('>>> 크로스 시작')
+        jsonList = []
+        jsonList.clear()
+        html = requests.get('http://made-club.com/sports/cross', headers=header)
+        bs4 = BeautifulSoup(html.text, 'lxml')
+        table = bs4.find('table',id='gameListTable')
+        trs = table.find_all('tr')
+        for tr in trs:
+            data = {}
+            try:
+                league = tr.find('th').get_text().strip()
+            except:
+                pass
+            try:
+                tc = tr.find('td',class_='tc').get_text()
+                ts = tr.find_all('td')[1]['data-ts']
+                leagueType = tr.find_all('td')[1]['data-league_type']
+                dcode = tr.find_all('td')[1]['data-dcode']
+                homeTeam = tr.find_all('td')[1]['data-team']
+                homeRatio = tr.find_all('td')[1]['data-odds']
+                drawRatio = tr.find_all('td')[4]['data-odds']
+                awayRatio = tr.find_all('td')[5]['data-odds']
+                awayTeam = tr.find_all('td')[5]['data-team']
+                data["League"] = league
+                data["DateTime"] = tc
+                data["HomeTeam"] = homeTeam
+                data["HomeRatio"] = homeRatio
+                data["DrawRatio"] = drawRatio
+                data["AwayRatio"] = awayRatio
+                data["AwayTeam"] = awayTeam
+                data["data-ts"] = ts
+                data["data-league_type"] = leagueType
+                data["data-dcode"] = dcode
+                jsonList.append(data)
+            except:
+                pass
+        root['cross'] = jsonList
+
+        # 실시간
+        print('>>> 실시간 시작')
+        html = requests.get('http://made-club.com/sports/live', headers=header)
+        bs4 = BeautifulSoup(html.text, 'lxml')
+        table = bs4.find('table', id='gameListTable')
+        trs = table.find_all('tr')
+        jsonList = []
+        jsonList.clear()
+        for tr in trs:
+            data = {}
+            try:
+                league = tr.find('th').get_text().strip()
+            except:
+                pass
+            try:
+                tc = tr.find('td', class_='tc').get_text()
+                ts = tr.find_all('td')[1]['data-ts']
+                leagueType = tr.find_all('td')[1]['data-league_type']
+                dcode = tr.find_all('td')[1]['data-dcode']
+                homeTeam = tr.find_all('td')[1]['data-team']
+                homeRatio = tr.find_all('td')[1]['data-odds']
+                drawRatio = tr.find_all('td')[4]['data-odds']
+                awayRatio = tr.find_all('td')[5]['data-odds']
+                awayTeam = tr.find_all('td')[5]['data-team']
+                data["League"] = league
+                data["DateTime"] = tc
+                data["HomeTeam"] = homeTeam
+                data["HomeRatio"] = homeRatio
+                data["DrawRatio"] = drawRatio
+                data["AwayRatio"] = awayRatio
+                data["AwayTeam"] = awayTeam
+                data["data-ts"] = ts
+                data["data-league_type"] = leagueType
+                data["data-dcode"] = dcode
+                jsonList.append(data)
+            except:
+                pass
+        root['live'] = jsonList
+        # 스페셜
+        print('>>> 스페셜 시작')
+        html = requests.get('http://made-club.com/sports/special', headers=header)
+        bs4 = BeautifulSoup(html.text, 'lxml')
+        table = bs4.find('table', id='gameListTable')
+        trs = table.find_all('tr')
+        jsonList = []
+        jsonList.clear()
+        for tr in trs:
+            data = {}
+            try:
+                league = tr.find('th').get_text().strip()
+            except:
+                pass
+            try:
+                tc = tr.find('td', class_='tc').get_text()
+                ts = tr.find_all('td')[1]['data-ts']
+                leagueType = tr.find_all('td')[1]['data-league_type']
+                dcode = tr.find_all('td')[1]['data-dcode']
+                homeTeam = tr.find_all('td')[1]['data-team']
+                homeRatio = tr.find_all('td')[1]['data-odds']
+                drawRatio = tr.find_all('td')[4]['data-odds']
+                awayRatio = tr.find_all('td')[5]['data-odds']
+                awayTeam = tr.find_all('td')[5]['data-team']
+                data["League"] = league
+                data["DateTime"] = tc
+                data["HomeTeam"] = homeTeam
+                data["HomeRatio"] = homeRatio
+                data["DrawRatio"] = drawRatio
+                data["AwayRatio"] = awayRatio
+                data["AwayTeam"] = awayTeam
+                data["data-ts"] = ts
+                data["data-league_type"] = leagueType
+                data["data-dcode"] = dcode
+                jsonList.append(data)
+            except:
+                pass
+        root['special'] = jsonList
+
+        f = open('JSON.txt', 'w', encoding='utf8')
+        f.write(json.dumps(root, ensure_ascii=False))
+        print(json.dumps(root, ensure_ascii=False))
+        print('>>> ',DELAY,'초후 다시시작')
+        time.sleep(DELAY)
+
 
