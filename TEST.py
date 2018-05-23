992139a6e8d6accd497cd65f4f7ea09d494b8852
DevHyung <khuphj@gmail.com>
2018-05-23 12:38:26 +0900

    All File upload

--- TEST.py
+++ TEST.py
@@ -3,192 +3,88 @@ import time
 import random
 if __name__=="__main__":
     driver = webdriver.Chrome('./chromedriver')
-    for qwweqweqweqwe in range(300):
+    for qwweqweqweqwe in range(1,100):
         print(qwweqweqweqwe, "번째 사람중 .. ")
-        driver.get(
-            'https://docs.google.com/forms/d/e/1FAIpQLSfrP2qAshSjLMPzfdzQhF9FAwZLHGAGIVk8xj9nVQ229uwHTw/viewform')
-        time.sleep(2)
-        lists = driver.find_elements_by_class_name('freebirdFormviewerViewItemsItemItem')
+        driver.get('https://form.office.naver.com/form/responseView.cmd?formkey=NzUyZTgyYjMtMDA5NC00Zjg4LTg2YjctMmVjMTExZGNkZjk3&sourceId=urlshare')
+        time.sleep(random.randint(2,4))
+
+        lists = driver.find_elements_by_class_name('formItemPh')
         answerList = []
-        age = ''
+
         ###1
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 10:
-            result = 0
-        elif tmp <= 40:
-            result = 1
-        elif tmp <= 75:
-            result = 2
-        elif tmp <= 95:
-            result = 3
-        else:
-            result = 4
-        answerList.append(result)
-        age = result
-        ###2
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 5:
-            result = 0
-        else:
-            result = 1
-        answerList.append(result)
-        ###3
-        if age != 0 or age != 4:
-            tmp = random.randint(0, 100000) % 100
-            if tmp <= 20:
-                result = 0
-            elif tmp <= 40:
-                result = 1
-            elif tmp <= 60:
-                result = 2
-            elif tmp <= 80:
-                result = 3
-            elif tmp <= 90:
-                result = 4
-            else:
-                result = 5
-        elif age == 0:
-            result = 0
-        else:
-            tmp = random.randint(0, 100000) % 100
-            if tmp <= 20:
-                result = 1
-            elif tmp <= 40:
-                result = 2
-            elif tmp <= 60:
-                result = 3
-            elif tmp <= 80:
-                result = 4
-            else:
-                result = 5
-        answerList.append(result)
-        ###4
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 10:
-            result = 0
-        elif tmp <= 60:
-            result = 1
-        elif tmp <= 95:
-            result = 2
-        else:
-            result = 3
-        answerList.append(result)
-        ###5
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 10:
-            result = 0
-        elif tmp <= 60:
-            result = 1
-        elif tmp <= 80:
-            result = 2
-        else:
-            result = 3
-        answerList.append(result)
-        ###6
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 20:
-            result = 0
-        else:
-            result = 1
-        answerList.append(result)
-        ###7
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 40:
-            result = 0
-        else:
-            result = 1
-        answerList.append(result)
-        ###7-1
-        if result == 0:
-            tmp = random.randint(0, 100000) % 100
-            if tmp <= 10:
-                result = 0
-            elif tmp <= 70:
-                result = 1
-            else:
-                result = 2
-        else:
-            result = -1
-        answerList.append(result)
-        ###8
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 40:
-            result = 0
-        else:
-            result = 1
-        answerList.append(result)
-        ###9
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 10:
-            result = 0
-        else:
-            result = 1
-        answerList.append(result)
-        ###10
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 5:
-            result = 0
-        elif tmp <= 30:
-            result = 1
-        elif tmp <= 70:
-            result = 2
-        else:
-            result = 3
-        answerList.append(result)
-        ###11
-        tmp = random.randint(0, 100000) % 100
-        if tmp <= 30:
-            result = 0
-        else:
-            result = 1
-        answerList.append(result)
-        ###11-1
-        if result == 0:
-            tmp = random.randint(0, 100000) % 100
-            if tmp <= 50:
-                result = 0
-            elif tmp <= 70:
-                result = 1
-            elif tmp <= 85:
-                result = 2
-            elif tmp <= 95:
-                result = 3
-            else:
-                result = 4
-        else:
-            result = -1
-        answerList.append(result)
-        ###12
         tmp = random.randint(0, 100000) % 100
         if tmp <= 90:
             result = 0
-        elif tmp <= 95:
+        elif tmp <= 98:
             result = 1
         else:
             result = 2
+
         answerList.append(result)
-        ###12-1
-        if result == 1:
-            tmp = random.randint(0, 100000) % 100
-            if tmp <= 40:
-                result = 0
-            elif tmp <= 60:
-                result = 1
-            elif tmp <= 70:
-                result = 2
-            elif tmp <= 75:
-                result = 3
-            elif tmp <= 85:
-                result = 4
-            else:
-                result = 5
+
+        ###2
+        bandal = 0
+        tmp = random.randint(0, 100000) % 100
+        if tmp <= 70:
+            result = 0
         else:
-            result = -1
+            result = 1
+        bandal = result
         answerList.append(result)
+        ###2-1
+        result = ''
+        if bandal == 0:
+            tmp = random.randint(0, 100000) % 100
+            if tmp <= 20:
+                result = '휴식'
+            elif tmp <= 30:
+                result = '데이트'
+            elif tmp <= 40:
+                result = '운동'
+            elif tmp <= 45:
+                result = '기타'
+            elif tmp <= 60:
+                result = '지나가는 경로'
+            elif tmp <= 80:
+                result = '여러목적으로 사용'
+            elif tmp <= 90:
+                result = '산책'
+            else:
+                result = '주로 가족들끼리'
+        answerList.append(result)
+        ###3
+        tmp = random.randint(0, 100000) % 100
+        if tmp <= 85:
+            result = 0
+        else:
+            result = 1
+        answerList.append(result)
+        ###4
+        tmp = random.randint(0, 100000) % 100
+        if tmp <= 60:
+            result = 0
+        else:
+            result = 1
+        answerList.append(result)
+        ###5
+        tmp = random.randint(0, 100000) % 100
+        if tmp <= 95:
+            result = 0
+        else:
+            result = 1
+        answerList.append(result)
+
         idx = 0
-        for list in lists:
-            if answerList[idx] != -1:
-                list.find_elements_by_class_name('exportLabelWrapper')[answerList[idx]].click()
-                time.sleep(0.1)
+        for li in lists:
+            if idx != 2:
+                li.find_elements_by_class_name('radio')[answerList[idx]].click()
+            else:
+                li.find_element_by_xpath('//*[@id="answer"]').click()
+                li.find_element_by_xpath('//*[@id="answer"]').send_keys(answerList[idx])
+            time.sleep(0.2)
             idx += 1
-        driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div[2]/div[3]/div/div/div/content/span').click()
+        try:
+            driver.find_element_by_xpath('//*[@id="pageNav"]/button[3]').click()
+        except:
+            print("헷")
+            pass
